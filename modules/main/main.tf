ca35878aa5eff9318baa3426557b1f6b7ed16878
DuckInCyber <37802070+DuckInCyber@users.noreply.github.com>
2022-09-18 12:47:22 +0300

    comment the version

--- modules/main/main.tf
+++ modules/main/main.tf
@@ -16,7 +16,7 @@ resource "helm_release" "this" {
   namespace  = var.k8s_namespace
   repository = var.repository
   chart      = var.chart
-  version    = var.chart_version
+  #version    = var.chart_version
 
   values = [templatefile("${path.module}/values.yaml", {
     ingress         = var.ingress
